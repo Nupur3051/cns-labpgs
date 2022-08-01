347576abe957ed8993cf361ea775b7b215b6c013
Nupur3051 <94536927+Nupur3051@users.noreply.github.com>
2022-08-01 13:57:06 +0530

    Update vigenere.c

--- vigenere.c
+++ vigenere.c
@@ -10,7 +10,7 @@ int main(){
     
     int msgLen = strlen(msg), keyLen = strlen(key), i, j;
  
-    char newKey[msgLen], encryptedMsg[msgLen]; 
+    char newKey[msgLen], encryptedMsg[msgLen],decryptedMsg[msgLen]; 
     //generating new key
     for(i = 0, j = 0; i < msgLen; ++i, ++j){
         if(j == keyLen)j = 0;
@@ -24,12 +24,20 @@ int main(){
         encryptedMsg[i] = ((msg[i] + newKey[i]) % 26) + 'A';
  
     encryptedMsg[i] = '\0';
+    //DECRYPTION
+    for(i = 0; i < msgLen; ++i)
+        decryptedMsg[i] = (((encryptedMsg[i] - newKey[i]) + 26) % 26) + 'A';
  
+    decryptedMsg[i] = '\0';
     printf("Original Message: %s", msg);
     printf("\nKey: %s", key);
     printf("\nNew Generated Key: %s", newKey);
     printf("\nEncrypted Message: %s", encryptedMsg);
+    printf("\nDecrypted Message: %s", decryptedMsg);
+    
 return 0;
 }
+
+
 
 
